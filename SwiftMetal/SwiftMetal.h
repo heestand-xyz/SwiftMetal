3156aade3e189efa3158293096f3e448bfc8bc72
Hexagons <anton.heestand@gmail.com>
2019-12-06 09:38:57 +0100

    Initial Commit

---
+++ SwiftMetal/SwiftMetal.h
@@ -0,0 +1,19 @@
+//
+//  SwiftMetal.h
+//  SwiftMetal
+//
+//  Created by Anton Heestand on 2019-12-06.
+//  Copyright © 2019 Hexagons. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftMetal.
+FOUNDATION_EXPORT double SwiftMetalVersionNumber;
+
+//! Project version string for SwiftMetal.
+FOUNDATION_EXPORT const unsigned char SwiftMetalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftMetal/PublicHeader.h>
+
+
