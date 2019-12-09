e248131922d75a96a78abfd5eaf921922480716d
Hexagons <anton.heestand@gmail.com>
2019-12-09 11:12:22 +0100

    demo

---
+++ SwiftMetalDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftMetalDemo
+//
+//  Created by Anton Heestand on 2019-12-09.
+//  Copyright © 2019 Hexagons. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
