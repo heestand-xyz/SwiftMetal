f47ac70cee394e0ba0961f2e4e9bfb9273687284
Hexagons <anton@hexagons.se>
2019-12-09 21:13:02 +0100

    test

--- SwiftMetalTests/SwiftMetalTests.swift
+++ SwiftMetalTests/SwiftMetalTests.swift
@@ -24,33 +24,28 @@ class SwiftMetalTests: XCTestCase {
         renderer = nil
     }
 
-    func testFunc() {
+    func testShader() {
         
         let shader = SMShader { uv in
-            var v = float4(0.0, 0.0, 0.0, 0.0)
-            for _ in 0..<5 {
-                v += float4(0.1, 0.2, 0.3, 0.4)
-            }
-            let c: SMFloat4 = v
-            return c
+            return float4(uv.x, uv.y, 0.0, 1.0)
         }
         print(shader.code())
 
-        let res = CGSize(width: 1, height: 1)
-        let render: SMTexture = try! renderer.render(shader, at: res, as: .rgba32Float)
+        let res = CGSize(width: 8, height: 8)
+        let render: SMTexture = try! renderer.render(shader, at: res, as: .rgba16Float)
         
         if let raw8 = try? render.raw8() {
-            if raw8.count == 4 {
+            if raw8.count <= 256 {
                 print("raw8", raw8.map({ CGFloat($0) / 255 }))
             }
             XCTAssertNotEqual(raw8.first!, 0)
         } else if let raw16 = try? render.raw16() {
-            if raw16.count == 4 {
+            if raw16.count <= 256 {
                 print("raw16", raw16)
             }
             XCTAssertNotEqual(raw16.first!, 0.0)
         } else if let raw32 = try? render.raw32() {
-            if raw32.count == 4 {
+            if raw32.count <= 256 {
                 print("raw32", raw32)
             }
             XCTAssertNotEqual(raw32.first!, 0.0)
