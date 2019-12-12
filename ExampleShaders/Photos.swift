b0920a46282e4a3c48c9f96cc92e6f95fe424c97
Hexagons <anton.heestand@gmail.com>
2019-12-12 16:17:54 +0100

    photos example

---
+++ ExampleShaders/Photos.swift
@@ -0,0 +1,45 @@
+//
+//  Photos.swift
+//  SwiftMetal_iOS
+//
+//  Created by Anton Heestand on 2019-12-12.
+//  Copyright © 2019 Hexagons. All rights reserved.
+//
+
+import SwiftUI
+import SwiftMetal
+
+let photosShader: SMShader = SMShader { uv in
+    let photo1 = SMTexture(image: UIImage(named: "photo1")!)!
+    let photo2 = SMTexture(image: UIImage(named: "photo2")!)!
+    let mask: SMBool = photo2.r < 0.1
+    return mask <?> photo1 <=> photo2
+}
+
+struct PhotosView: View {
+    var body: some View {
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            VStack(spacing: 0) {
+                HStack(spacing: 0) {
+                    Image("photo1")
+                        .resizable()
+                        .aspectRatio(1.5, contentMode: .fit)
+                    Image("photo2")
+                        .resizable()
+                        .aspectRatio(1.5, contentMode: .fit)
+                }
+                SMView { photosShader }
+                    .aspectRatio(1.5, contentMode: .fit)
+            }
+        }
+    }
+}
+
+struct PhotosView_Previews: PreviewProvider {
+    static var previews: some View {
+        PhotosView()
+            .previewLayout(.fixed(width: 750, height: 750))
+    }
+}
